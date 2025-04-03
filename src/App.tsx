cc2c24e5142432b2b81c3a77b268aeb55b3a271a
Boris <lightwaveinthespace@gmail.com>
2025-04-03 18:44:28 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>It's working</h1>
       <div className="card">
         <canvas
           ref={(el) => (ref.current = el)}
